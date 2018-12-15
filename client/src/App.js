e24d74b15ca61ea3c70e1161e0504ebd5d20c4ea
Silas Burger <ybsilas@gmail.com>
2018-12-15 15:41:45 -0800

    started

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,65 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const data = []
+data.push(createCard('What color is the sky?', 'Go play outside'))
+data.push(createCard('What color is the ocean?', 'Blue'))
+data.push(createCard('Why?', 'Go watch tv'))
+
+function createCard(q, a) {
+  // add an uuid id
+  return {
+    question: q,
+    answer: a,
+    ratings: {
+      easy: 0,
+      medium: 0,
+      hard: 0,
+    }
+  }
+}
+
 class App extends Component {
+  state = {
+    priorityQueue = []
+  }
+
+  componentDidMount() {
+    this.setState(prev => {
+      prev.priorityQueue.push(...data)
+    })
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Card />
+        <Buttons />
       </div>
     );
   }
 }
 
+
+class Card extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+
+class Buttons extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
 export default App;
