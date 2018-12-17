4a0aa0f41d8461d4bcc0cb03622f90f8e914a740
Alex Bush <alexabush7@gmail.com>
2018-12-17 10:14:45 -0800

    removed unused files, added currentCard to state

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import uuid from 'uuidv4'
 import './App.css';
 
-const data = []
-data.push(createCard('What color is the sky?', 'Go play outside'))
-data.push(createCard('What color is the ocean?', 'Blue'))
-data.push(createCard('Why?', 'Go watch tv'))
+const cardsData = [];
+cardsData.push(createCard('What color is the sky?', 'Go play outside'));
+cardsData.push(createCard('What color is the ocean?', 'Blue'));
+cardsData.push(createCard('Why?', 'Go watch tv'));
 
 function createCard(q, a) {
   // add an uuid id
   return {
+    id: uuid(),
     question: q,
     answer: a,
     ratings: {
       easy: 0,
       medium: 0,
-      hard: 0,
-    }
-  }
+      hard: 0
+    },
+    priorityScore: 0
+  };
 }
 
 class App extends Component {
   state = {
-    priorityQueue = []
-  }
+    currentCard: {},
+    priorityQueue: []
+  };
 
   componentDidMount() {
-    this.setState(prev => {
-      prev.priorityQueue.push(...data)
-    })
+    // api/data
+    this.setState(() => {
+      let newQueue = [];
+      for (let card of cardsData) {
+        newQueue.push(card);
+      }
+      let newCard = newQueue.shift();
+      return { priorityQueue: newQueue, currentCard: newCard };
+    });
   }
 
+  changeRating = (cardId, rating = 'hard') => {
+    // make patch to server with updated rating
+
+    // push to priority queue with new rating and 'priorityScore'
+  };
 
   render() {
+    console.log('this.state', this.state)
     return (
       <div>
-        <Card />
+        <Card data={this.state.currentCard} />
         <Buttons />
       </div>
     );
   }
 }
 
-
 class Card extends Component {
   render() {
+    let { question, answer, id } = this.props.data;
     return (
       <div>
-
+        <p>{question}</p>
+        <p>{answer}</p>
       </div>
     );
   }
 }
 
-
 class Buttons extends Component {
   render() {
-    return (
-      <div>
-
-      </div>
-    );
+    return <div />;
   }
 }
 export default App;
