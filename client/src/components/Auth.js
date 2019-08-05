a435c5fd70a02c3c29315c215f6bc5b2ed6b74e2
Alexander Bush <alexabush7@gmail.com>
2019-08-05 18:33:58 -0500

    removed client completely to see if plain server will run

--- client/src/components/Auth.js
+++
@@ -1,9 +0,0 @@
-import React, { PureComponent } from 'react';
-
-class Auth extends PureComponent {
-  render() {
-    return <div className="Auth">Auth</div>;
-  }
-}
-
-export default Auth;
--- client/src/helpers/calcPriority.js
+++
@@ -1,6 +0,0 @@
-function calcPriority(easy = 0, medium = 0, hard = 0) {
-  let priorityScore = easy * 10 + medium * 5 + hard * 1;
-  return priorityScore;
-}
-
-export default calcPriority
