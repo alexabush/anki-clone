462ba84edb1b5f0828e8b50f2e860899ecf32371
Alexander Bush <alexabush7@gmail.com>
2019-08-05 18:56:42 -0500

    messing with environment configs

--- app.js
+++ app.js
@@ -19,8 +19,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', function(req, res) {
+  res.send(JSON.stringify({ Hello: 'World' }));
+});
 app.use('/api', api);
 console.log('in app.js of server');
 
